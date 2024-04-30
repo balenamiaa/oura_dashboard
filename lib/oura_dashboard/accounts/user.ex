f0120ee04e139a52d06ff2ee67dfaf41ed1cf051
Balen <barena.ekusuimii@gmail.com>
2024-04-30 19:53:57 +0300

    security bug fix (oop)

--- lib/oura_dashboard/accounts/user.ex
+++ lib/oura_dashboard/accounts/user.ex
@@ -2,7 +2,7 @@ defmodule OuraDashboard.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, except: [:__meta__]}
+  @derive {Jason.Encoder, except: [:__meta__, :password, :hashed_password]}
 
   schema "users" do
     field(:email, :string)
