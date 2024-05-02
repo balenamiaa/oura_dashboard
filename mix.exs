73235ae634b5b4889f32b9463ff86f19c208fc43
Balen <barena.ekusuimii@gmail.com>
2024-05-02 10:34:18 +0300

    change readme.md + depend on oura_api with git

--- mix.exs
+++ mix.exs
@@ -52,7 +52,7 @@ defmodule OuraDashboard.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:oura_api, path: "../oura_api"},
+      {:oura_api, git: "git@github.com:balenamiaa/oura_api", branch: "main"},
       {:live_svelte, "~> 0.13.1"}
     ]
   end
